8830a8da8a25f0cf73c9204e04ba3561f9d35764
Thom Joy <thomjoy1984@gmail.com>
2015-03-02 23:35:49 +1100

    update queries

--- gtfs_syd.sql
+++ gtfs_syd.sql
@@ -40,7 +40,7 @@ WHERE trip_id IN (SELECT DISTINCT trip_id FROM stop_times WHERE stop_id = 200076
 
 SELECT *
 FROM shapes 
-WHERE shape_id = 157535;
+WHERE shape_id = 157535;x
 
 /* view for populating the 'arriving soon' tab */
 SELECT t.route_id, t.service_id, t.trip_id, t.trip_headsign, t.direction_id 
@@ -84,22 +84,53 @@ FROM stop_times st
 JOIN stops s ON s.stop_id = st.stop_id
 JOIN trips t ON t.trip_id = st.trip_id
 JOIN calendar c ON c.service_id = t.service_id
-WHERE t.route_id IN (
-	SELECT route_id 
-	FROM trips 
-	WHERE route_id = '11954_333'
-	)
+WHERE t.trip_id = '76466985_20150127_11954'
 AND t.service_id IN (
 	SELECT c1.service_id
 	FROM calendar c1
 	WHERE c1.wednesday = 1
 	)
-AND date_trunc('minute', st.departure_time::time) - date_trunc('minute', now()::time) <= (interval '10 minute') 
-AND date_trunc('minute', st.departure_time::time) - date_trunc('minute', now()::time) > (interval '0 minute') 
 ORDER BY t.direction_id, st.stop_sequence;
 
-SELECT *
-FROM shapes 
-WHERE shape_id = 
+SELECT st.trip_id, st.departure_time, t.route_id, t.shape_id, t.trip_headsign
+FROM stop_times st
+JOIN trips t ON t.trip_id = st.trip_id
+JOIN calendar c ON c.service_id = t.service_id
+WHERE st.stop_id = 202161
+AND t.service_id IN (SELECT c1.service_id
+FROM calendar c1
+WHERE c1.monday = 1)
+AND date_trunc('minute', st.departure_time::time) - date_trunc('minute', now()::time) <= (interval '5 minute') 
+AND date_trunc('minute', st.departure_time::time) - date_trunc('minute', now()::time) > (interval '0 minute') 
+ORDER BY st.departure_time ASC
 
-SELECT greatest(-'1 hour'::interval, '1 hour'::interval);
+/* find the nearest stop in a radius where there is a service departing in the next X minutes */
+SELECT st.stop_id, s.stop_lat, s.stop_lon, st.departure_time
+FROM stops s 
+JOIN stop_times st ON st.stop_id = s.stop_id
+JOIN trips t ON t.trip_id = st.trip_id
+AND t.service_id IN (SELECT c1.service_id
+FROM calendar c1
+WHERE c1.monday = 1)
+AND date_trunc('minute', st.departure_time::time) - date_trunc('minute', now()::time) <= (interval '5 minute') 
+AND date_trunc('minute', st.departure_time::time) - date_trunc('minute', now()::time) > (interval '0 minute') 
+
+/* update the stops table to use postgis geometry types */
+CREATE EXTENSION postgis;
+SELECT AddGeometryColumn ('stops', 'geom', 4326, 'POINT', 2);
+UPDATE stops SET geom = ST_Setsrid(ST_Makepoint(stop_lon, stop_lat),4326);
+
+/* find all stops around a point - where there is service leaving in X minutes */
+SELECT s.stop_name, s.stop_lat, s.stop_lon
+FROM stops s 
+JOIN stop_times st ON st.stop_id = s.stop_id
+JOIN trips t ON t.trip_id = st.trip_id
+AND t.service_id IN (SELECT c1.service_id
+FROM calendar c1
+WHERE c1.monday = 1)
+WHERE ST_Distance_Sphere(geom, ST_MakePoint(151.22639894485471, -33.88468522118266)) <= ((10 / 0.62137) / 100) * 1609.34
+AND date_trunc('minute', st.departure_time::time) - date_trunc('minute', now()::time) <= (interval '10 minute') 
+AND date_trunc('minute', st.departure_time::time) - date_trunc('minute', now()::time) > (interval '0 minute') 
+
+
+
